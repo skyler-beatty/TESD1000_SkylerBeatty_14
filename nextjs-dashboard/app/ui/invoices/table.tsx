b16829ed57e9cd07a5021a8c14bfed5db0242633
Skyler Beatty <109917545+skyler-beatty@users.noreply.github.com>
2026-03-24 21:03:25 -0600

    Update table.tsx

--- nextjs-dashboard/app/ui/invoices/table.tsx
+++ nextjs-dashboard/app/ui/invoices/table.tsx
@@ -17,7 +17,13 @@ export default async function InvoicesTable({ query, currentPage }: { query: str
 								<div className="flex items-center justify-between border-b pb-4">
 									<div>
 										<div className="mb-2 flex items-center">
-											<Image src={invoice.image_url} className="mr-2 rounded-full" width={28} height={28} />
+											<Image
+												src={invoice.image_url}
+												className="rounded-full"
+												width={28}
+												height={28}
+												alt={`${invoice.name}'s profile picture`}
+											/>
 											<p>{invoice.name}</p>
 										</div>
 										<p className="text-sm text-gray-500">{invoice.email}</p>
